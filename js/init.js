706f7bd00037afdf306a2b952a22e6a42d7d2a1d
Pramo <Pramo@192.168.1.8>
2015-04-22 00:34:15 +0800

    Merge origin/master
    Conflicts:
    	js/init.js

--- js/init.js
+++ js/init.js
@@ -5,15 +5,6 @@
     $('.parallax').parallax();
     $('select').material_select();
      $('.slider').slider({full_width: false});
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('select').material_select();
     $('.modal-trigger').leanModal();
 
   }); // end of document ready
