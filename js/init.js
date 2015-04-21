8be991f6deb22401cbd9a430ca258f4162acc1e0
Aruna Tebel <arunatebel@hotmail.com>
2015-04-22 00:07:09 +0530

    Made responsive

--- js/init.js
+++ js/init.js
@@ -1,11 +1,25 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('select').material_select();
-     $('.slider').slider({full_width: false});
-    $('.modal-trigger').leanModal();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('select').material_select();
+        $('.slider').slider({full_width: false});
+        $('.modal-trigger').leanModal();
 
-  }); // end of document ready
+        function doSomething() {
+            if ($(window).width() <= 850) {
+//                $("#logo-image").attr('src', 'images/icons/facebook-icon.png');
+            }else{
+//                $("#logo-image").attr('src', 'images/logo.png');
+            }
+        }
+
+        var resizeTimer;
+        $(window).resize(function () {
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(doSomething, 100);
+        });
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
